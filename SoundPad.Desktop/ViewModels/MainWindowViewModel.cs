6120edf297b5e78b83eb6c2e12baab7c5ec7241d
lbelloq <lbelloq@gmail.com>
2026-06-19 16:49:10 -0400

    Refactored playback management: added `DisposeTransientResources` for resource cleanup, improved Play-stream handling with null check, and centralized cleanup logic in Stop and Dispose methods.

--- SoundPad.Desktop/ViewModels/MainWindowViewModel.cs
+++ SoundPad.Desktop/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -26,25 +27,13 @@ public partial class MainWindowViewModel : ViewModelBase, IDisposable
     {
         _engine = new MiniAudioEngine();
         _engine.UpdateAudioDevicesInfo();
-        var defaultDevice = _engine.PlaybackDevices.FirstOrDefault(x => x.IsDefault);
-        _device = _engine.InitializePlaybackDevice(defaultDevice, _FORMAT);
+        var default_device = _engine.PlaybackDevices.FirstOrDefault(x => x.IsDefault);
+        _device = _engine.InitializePlaybackDevice(default_device, _FORMAT);
     }
 
-    [RelayCommand]
-    private void Play()
+    private void DisposeTransientResources()
     {
-        var assembly = Assembly.GetExecutingAssembly();
-        _stream = assembly.GetManifestResourceStream("SoundPad.Desktop.Resources.biggamehunter.wav")!;
-        _provider = new StreamDataProvider(_engine, _FORMAT, _stream);
-        _player = new SoundPlayer(_engine, _FORMAT, _provider);
-        _device.MasterMixer.AddComponent(_player);
-        _device.Start();
-        _player.Play();
-    }
-
-    [RelayCommand]
-    private void Stop()
-    {
+        Debug.WriteLine("DisposeTransientResources");
         if (_player != null)
         {
             _player.Stop();
@@ -57,13 +46,36 @@ public partial class MainWindowViewModel : ViewModelBase, IDisposable
         _stream?.Dispose();
         _stream = null;
     }
+
+    [RelayCommand]
+    private void Play()
+    {
+        DisposeTransientResources();
+
+        var assembly = Assembly.GetExecutingAssembly();
+        _stream = assembly.GetManifestResourceStream("SoundPad.Desktop.Resources.biggamehunter.wav");
+        if (_stream == null)
+        {
+            throw new InvalidOperationException("Embedded resource not found.");
+        }
+
+        _provider = new StreamDataProvider(_engine, _FORMAT, _stream);
+        _player = new SoundPlayer(_engine, _FORMAT, _provider);
+        _device.MasterMixer.AddComponent(_player);
+        _device.Start();
+        //_player.PlaybackEnded += (_, _) => Stop();
+        _player.Play();
+    }
+
+    [RelayCommand]
+    private void Stop()
+    {
+        DisposeTransientResources();
+    }
     
     public void Dispose()
     {
-        if (_player != null)
-        {
-            Stop();
-        }
+        DisposeTransientResources();
         _device.Stop();
         _device.Dispose();
         _engine.Dispose();
