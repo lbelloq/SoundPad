756d3e8bd097229107d2085ed49ada447f1bf9bf
lbelloq <lbelloq@gmail.com>
2026-06-19 23:05:44 -0400

    Make `Path` in `PadButton` non-partial, trigger `OnPropertyChanged` for `Path` and `PathString`.

--- SoundPad.Desktop/Models/PadButton.cs
+++ SoundPad.Desktop/Models/PadButton.cs
@@ -5,12 +5,21 @@ using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace SoundPad.Desktop.Models;
 
-public partial class PadButton : ObservableObject
+public class PadButton : ObservableObject
 {
     public int ButtonId { get; set; }
 
-    [ObservableProperty] 
-    public partial string? Path { get; set; }
+    private string? _path;
+    public string? Path
+    {
+        get => _path;
+        set
+        {
+            _path = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(PathString));
+        }
+    }
 
     public string PathString => Path ?? "Unassigned";
 
